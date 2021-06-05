4b876ecd09a5a15f6e179bf0ba99514766f1ae5a
HieuTong <hieu.le@coinhako.com>
2021-06-06 00:23:35 +0700

    Track privacy

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import AppTrackingTransparency
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Get permission", for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton() {
+        ATTrackingManager.requestTrackingAuthorization { (status) in
+            switch status {
+            case .notDetermined:
+                break
+            case .restricted:
+                break
+            case .denied:
+                break
+            case .authorized:
+                print("Allowed")
+                break
+            @unknown default:
+                break
+            }
+        }
     }
 
 
